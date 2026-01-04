a784c7fe87152e9983dc85f46d36d81c3296ef99
HUMUM <humumshaikh0@gmail.com>
2026-01-04 12:39:50 +0530

    Add files via upload

---
+++ half_adder/halfAdder_pkg.sv
@@ -0,0 +1,15 @@
+package halfAdder_pkg;
+
+    `include "uvm_macros.svh"
+    import uvm_pkg::*;
+
+    `include "seq_item.sv"
+    `include "base_seq.sv"
+    `include "sequencer.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+    `include "agent.sv"
+    `include "scoreboard.sv"
+    `include "environment.sv"
+
+endpackage : halfAdder_pkg
