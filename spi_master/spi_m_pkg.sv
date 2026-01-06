9a1c3159bf79310059bc9419f945cbe27c9aaef1
HUMUM <humumshaikh0@gmail.com>
2026-01-06 20:44:51 +0530

    Add files via upload

---
+++ spi_master/spi_m_pkg.sv
@@ -0,0 +1,15 @@
+package spi_m_pkg;
+
+    `include "uvm_macros.svh"
+    import uvm_pkg::*;
+
+    `include "seq_item.sv"
+    `include "sequence.sv"
+    `include "sequencer.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+    `include "agent.sv"
+    `include "scoreboard.sv"
+    `include "environment.sv"
+
+endpackage : spi_m_pkg
