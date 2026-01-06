035daf6dd9c4c65364d23fa4a39e7563cc59f5aa
HUMUM <humumshaikh0@gmail.com>
2026-01-07 00:01:49 +0530

    Add files via upload

---
+++ xor_gate/sequence.sv
@@ -0,0 +1,22 @@
+class xor_sequence extends uvm_sequence #(xor_seq_item);
+
+    `uvm_object_utils(xor_sequence)
+
+    xor_seq_item    I;
+
+    function new(string name = "xor_sequence");
+        super.new();
+        `uvm_info("Sequence Class" , "Constructor" , UVM_LOW)
+    endfunction : new
+
+
+    task body();
+        repeat(8)
+        begin
+            I = xor_seq_item::type_id::create("I");
+            `uvm_do(I)
+        end
+    endtask : body
+
+
+endclass : xor_sequence
