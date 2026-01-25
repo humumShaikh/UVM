1df2d407566036b6fc31f690d8c9f2fbae6cf209
HUMUM <humum.shaikh@outlook.com>
2026-01-25 17:19:43 +0530

    Update interface.sv

--- xor_gate/interface.sv
+++ xor_gate/interface.sv
@@ -1,10 +1,11 @@
 interface XOR_INTF
 (
-    input logic clk
+    input logic clk                //signal that will be passed from the top module
 );
 
     logic   A;
     logic   B;
     logic   Y;
+
 
 endinterface : XOR_INTF
