5411b655365376eb92ccb861f9bd71e1b89a3407
HUMUM <humum.shaikh@outlook.com>
2026-01-26 12:04:19 +0530

    Update monitor.sv

--- xor_gate/monitor.sv
+++ xor_gate/monitor.sv
@@ -28,8 +28,8 @@ class xor_monitor extends uvm_monitor;                                        //
         begin
             @(posedge vif.clk);                                                        //wait till the posedge of clk is detected in the virtual interface
             #1ns;                                                                    //wait 1ns
-            `uvm_info("Monitor Class" , "Run Phase" , UVM_LOW)
-            I = xor_seq_item::type_id::create("I");
+            `uvm_info("Monitor Class" , "Run Phase" , UVM_LOW)                            //print info message using uvm_macro : `uvm_info
+            I = xor_seq_item::type_id::create("I");        
             I.A = vif.A;
             I.B = vif.B;
             I.Y = vif.Y;
@@ -39,6 +39,7 @@ class xor_monitor extends uvm_monitor;                                        //
 
 
 endclass : xor_monitor
+
 
 
 
