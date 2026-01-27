3b332c6527180059988cf4490d42195b2bf9e595
HUMUM <humum.shaikh@outlook.com>
2026-01-27 22:55:25 +0530

    Update scoreboard.sv

--- xor_gate/scoreboard.sv
+++ xor_gate/scoreboard.sv
@@ -8,7 +8,7 @@ class xor_scoreboard extends uvm_scoreboard;                                    
 
     xor_seq_item    I;                                                                //creating a sequence item handle and calling it as 'I'
 
-    function new(string name = "xor_scoreboard" , uvm_component parent);
+    function new(string name = "xor_scoreboard" , uvm_component parent);                //constructor for xor_scoreboard class
         super.new(name , parent);
         `uvm_info("Scoreboard Class" , "Constructor" , UVM_LOW)
     endfunction : new
@@ -46,6 +46,7 @@ class xor_scoreboard extends uvm_scoreboard;                                    
 
 
 endclass : xor_scoreboard
+
 
 
 
