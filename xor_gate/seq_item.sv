97327945ac33a44ddbda8ffec1863249075712d9
CWIR3 <humum.shaikh@outlook.com>
2026-02-10 23:46:42 +0530

    Refactor xor_seq_item class definition

--- xor_gate/seq_item.sv
+++ xor_gate/seq_item.sv
@@ -1,6 +1,6 @@
-class xor_seq_item extends uvm_sequence_item;
+class xor_seq_item extends uvm_sequence_item;        //xor_seq_item extending from uvm_sequence_item, it's like the transaction class of SV
 
-    `uvm_object_utils(xor_seq_item)
+    `uvm_object_utils(xor_seq_item)                //UVM factory registration for this class
 
     rand    bit A;
     rand    bit B;
@@ -11,5 +11,6 @@ class xor_seq_item extends uvm_sequence_item;
         `uvm_info("Sequence Item Class" , "Constructor" , UVM_LOW)
     endfunction : new
     
+
 
 endclass : xor_seq_item
